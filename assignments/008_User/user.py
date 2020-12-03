df5d239809d2acecd1487f4e2113986630b94843
IlScottlI <scotteverettjohnson84@gmail.com>
2020-12-03 08:49:13 -0500

    Added Chaining Methods

--- assignments/008_User/user.py
+++ assignments/008_User/user.py
@@ -17,23 +17,28 @@ class User:
     def make_deposit(self, amount):
         self.account_balance += amount
         print(f'Deposit was made: ${float(amount)}')
+        return self
+
 # make_withdrawal(self, amount) - have this method decrease the user's balance by the amount specified
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
         print(f'Withdrawal was made: (${float(amount)})')
+        return self
 # display_user_balance(self) - have this method print the user's name and account balance to the terminal
 
     def info(self):
         print(
             f"Name: {self.name}, Email: {self.email}, UserName: {self.username}, AccountBalance: ${self.account_balance}"
         )
+        return self
 
 # BONUS: transfer_money(self, other_user, amount) - have this method decrease the user's balance by the amount and add that amount to other other_user's balance
 
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 
 Scott = User('Scott Johnson', 'johnson.se@pg.com', 'johnson.se')
@@ -48,3 +53,6 @@ Jim.make_deposit(150)
 Scott.transfer_money(Jim, 52.78)
 Scott.info()
 Jim.info()
+
+Scott.make_deposit(100).make_deposit(
+    200).make_deposit(300).make_withdrawal(50).info()
