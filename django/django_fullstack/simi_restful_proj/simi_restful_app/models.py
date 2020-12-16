400eb96a8241567f69ede5e207fd348e7298671e
IlScottlI <scotteverettjohnson84@gmail.com>
2020-12-16 14:00:14 -0500

    Assignment: Semi-Restful TV Shows Validated

--- django/django_fullstack/simi_restful_proj/simi_restful_app/models.py
+++ django/django_fullstack/simi_restful_proj/simi_restful_app/models.py
@@ -1,10 +1,47 @@
 from django.db import models
+from datetime import datetime, timedelta
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        try:
+            # add keys and values to errors dictionary for each invalid field
+            if len(postData['network']) < 3:
+                errors["network"] = "Network should be at least 3 characters"
+            if len(postData['description']) > 0:
+                if len(postData['description']) < 10:
+                    errors["description"] = "Description should be at least 10 characters"
+                else:
+                    pass
+            if len(postData['release_date']):
+                datetime_object = datetime.strptime(
+                    postData['release_date'], '%Y-%m-%d')
+                past = datetime_object - timedelta(days=-1)
+                present = datetime.now()
+                if past > present:
+                    errors["release_date"] = "Release Date should be in the past"
+
+            print(len(Show.objects.filter(title=postData['title'])) > 0)
+            if len(Show.objects.filter(title=postData['title'])) > 0:
+
+                if postData['type'] == 'new':
+                    print(Show.objects.filter(title=postData['title'])[0].id)
+                    errors['title'] = "That show already exists"
+                else:
+                    pass
+            if len(postData['title']) < 2:
+                errors["title"] = "Title name should be at least 2 characters"
+        except:
+            pass
+        return errors
 
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
     release_date = models.DateField()
-    description = models.TextField(default=None, blank=True, null=True)
+    description = models.TextField(default='', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- django/django_fullstack/simi_restful_proj/simi_restful_app/views.py
+++ django/django_fullstack/simi_restful_proj/simi_restful_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 
 def shows(request):
@@ -23,6 +24,7 @@ def view(request, show_id):
 
 def destroy(request, show_id):
     Show.objects.get(id=show_id).delete()
+    messages.success(request, "Show was Successfully Deleted")
     return redirect('/shows')
 
 
@@ -37,24 +39,13 @@ def edit(request, show_id):
 
 def process(request):
     action = redirect('/shows')
-    type_ = getSet(request, 'type')
+    type_ = request.POST['type']
+
     if type_ == 'new':
-        newItem = Show.objects.create(
-            title=getSet(request, 'title'),
-            network=getSet(request, 'network'),
-            release_date=getSet(request, 'release_date'),
-            description=getSet(request, 'description'),
-        )
-        action = redirect(f"/shows/{newItem.id}")
+        action = newForm(request)
     if type_ == 'update':
         show_id = getSet(request, 'show_id')
-        show_to_update = Show.objects.get(id=show_id)
-        show_to_update.title = getSet(request, 'title')
-        show_to_update.network = getSet(request, 'network')
-        show_to_update.release_date = getSet(request, 'release_date')
-        show_to_update.description = getSet(request, 'description')
-        show_to_update.save()
-        action = redirect(f"/shows/{show_id}")
+        action = updateForm(request, show_id)
     return action
 
 
@@ -71,3 +62,47 @@ def getSet(request, name):
             response = value
 
     return response
+
+
+def updateForm(request, id):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        action = redirect(f"/shows/{id}/edit")
+        return action
+    else:
+        if len(request.POST['description']) > 0:
+            description = request.POST['description']
+        else:
+            description = ''
+        show_to_update = Show.objects.get(id=id)
+        show_to_update.title = getSet(request, 'title')
+        show_to_update.network = getSet(request, 'network')
+        show_to_update.release_date = getSet(request, 'release_date')
+        show_to_update.description = description
+        show_to_update.save()
+        messages.success(request, "Show Successfully Updated")
+        action = redirect(f"/shows/{id}")
+        return action
+
+
+def newForm(request):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+
+            messages.error(request, value)
+        action = redirect(f"/shows/new")
+        return action
+    else:
+        messages.success(request, "Show Successfully Created")
+        # redirect to a success route
+        newItem = Show.objects.create(
+            title=getSet(request, 'title'),
+            network=getSet(request, 'network'),
+            release_date=getSet(request, 'release_date'),
+            description=getSet(request, 'description'),
+        )
+        action = redirect(f"/shows/{newItem.id}")
+        return action
